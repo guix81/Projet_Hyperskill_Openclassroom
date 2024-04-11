6b3e166bbadc8a20f9d558a6d8b9515209142af2
guix81 <40278673+guix81@users.noreply.github.com>
2024-04-11 14:06:44 +0200

    update

--- Projet_hyperskill_10/modules/fonctions.py
+++ Projet_hyperskill_10/modules/fonctions.py
@@ -7,6 +7,14 @@ class Equation:
     msg_3 = "Yeah... division by zero. Smart move..."
     msg_4 = "Do you want to store the result? (y / n):" 
     msg_5 = "Do you want to continue calculations? (y / n):"
+    msg_6 = " ... lazy"
+    msg_7 = " ... very lazy"
+    msg_8 = " ... very, very lazy"
+    msg_9 = "You are"
+    msg_10 = "Are you sure? It is only one digit! (y / n)"
+    msg_11 = "Don't be silly! It's just one number! Add to the memory? (y / n)"
+    msg_12 = "Last chance! Do you really want to embarrass yourself? (y / n)"
+    list_msg = [msg_10, msg_11, msg_12]
     list_result = [0]
 
     def __init__(self, chaine):  # equation chaine
@@ -15,6 +23,8 @@ class Equation:
         self.y = 0
         self.oper = ''
         self.result = 0.0
+        self.msg = ''
+        self.msg_index = 0
         self.list_bool = []  # validation des méthodes
 
     def init(self, obj):
@@ -22,6 +32,7 @@ class Equation:
         obj.tri_chaine()
         obj.verif_xy()
         obj.verif_oper()
+        obj.check()
         obj.calc()
         obj.store_result()
         #obj.display()
@@ -30,7 +41,10 @@ class Equation:
         self.chaine = self.chaine.split(' ')
 
     def verif_xy(self):
-        if self.chaine[0] == 'M':
+        if self.chaine[0] == 'M' and self.chaine[2] == 'M':
+            self.x = self.list_result[-1]
+            self.y = self.list_result[-1]
+        elif self.chaine[0] == 'M':
             self.x = self.list_result[-1]
         elif self.chaine[2] == 'M':
             self.y = self.list_result[-1]
@@ -56,20 +70,36 @@ class Equation:
                     print(self.msg_1)
                     self.list_bool.append(False)
                     return
-        self.list_bool.append(True)
+        self.list_bool.append(True)  # valide x, y
         
     def verif_oper(self):
         if False not in self.list_bool:
             list_sign = ['-', '+', '*', '/']
-
             if self.chaine[1] in list_sign:
                 self.oper = self.chaine[1]
-                self.list_bool.append(True)
-                return # valide x oper y
+                self.list_bool.append(True)  # valide oper
+                return
             else:
                 print(self.msg_2)
                 self.list_bool.append(False)
                 return
+            
+    def is_one_digit(self, xy):
+        if xy > -10 and xy < 10 and float(xy).is_integer():
+            return True
+        else:
+            return False
+
+    def check(self):
+        if self.is_one_digit(self.x) and self.is_one_digit(self.y):
+            self.msg = self.msg + self.msg_6
+        if (self.x == 1 or self.y == 1) and self.oper == '*':
+            self.msg = self.msg + self.msg_7
+        if (self.x == 0 or self.y == 0) and (self.oper == '*' or self.oper == '+' or self.oper == '-'):
+            self.msg = self.msg + self.msg_8
+        if self.msg != '':
+            self.msg = self.msg_9 + self.msg
+            print(self.msg)
         
     def calc(self):
         if False not in self.list_bool:
@@ -92,16 +122,34 @@ class Equation:
             else:
                 print(self.msg_3)
                 self.list_bool.append(False)
-                return
     
-    def store_result(self):
+    def store_result(self):  # mémorise le résultat
         if False not in self.list_bool:
+            rep_1 = ''
             while True:
+                if rep_1 == 'n':
+                    break
+                elif not self.msg_index < 2:
+                    break
                 print(self.msg_4)
                 rep = input()
                 if rep == 'y':
-                    self.list_result.append(self.list_result[-1] + self.result)
-                    break
+                    if self.is_one_digit(self.result):
+                        self.msg_index = 0
+                        while True:
+                            print(self.list_msg[self.msg_index])
+                            rep_1 = input()
+                            if rep_1 == 'y':
+                                if self.msg_index < 2:
+                                    self.msg_index += 1
+                                else:
+                                    self.list_result.append(self.result)
+                                    break
+                            elif rep_1 == 'n':
+                                break
+                    else:
+                        self.list_result.append(self.result)
+                        break
                 elif rep == 'n':
                     break
             while True:
@@ -112,7 +160,7 @@ class Equation:
                 elif rep == 'n':
                     sys.exit(1)
 
-    def display(self):
+    def display(self):  # debug
         print(self.chaine)
         print(self.x)
         print(self.y)
